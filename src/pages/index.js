87f8cceb1268fbfb9d1549a5842eaf557cc8a33b
TemuujinTMJ <hades.tmj@gmail.com>
2021-11-24 23:15:50 +0800

    gitcod

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/sidebar'
+import { useState } from 'react'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <>
-         <Sidebar />
-         <Navbar />
-            
+         <Sidebar isOpen={isOpen} toggle={toggle}/>
+         <Navbar toggle={toggle}/>
+         <HeroSection />
         </>
     )
 }
